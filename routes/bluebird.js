71d701f48174db11be9890248491bab8405b2cee
DAVID KRIER <dkflyboy@gmail.com>
2018-06-26 16:02:20 -0500

    Commit 2

--- routes/bluebird.js
+++ routes/bluebird.js
@@ -2,23 +2,31 @@ var express = require('express');
 var request = require('request'),
     sys = require('sys');
 
+var Promise = require('bluebird');
+
 var router = express.Router();
 var joke = "";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    request({ uri:'http://api.icndb.com/jokes/random?exclude=[nerdy,explicit]&limitTo=[nerdy]' }, function (error, response, body) {
-        if (!(error && response.statusCode !== 200)) {
-            var obj = JSON.parse(body);
-            joke = obj.value.joke;
-            res.render('chuck', { joke: joke });
-        } else {
-            console.log('Error when contacting the API')
-            joke = "ERROR";
-            res.render('chuck', { joke: joke });
+var addTwoPromise = function(num) {
+    return new Promise(
+        function (fulfill, reject) {
+           fulfill(num + 2);
         }
-    });
+     );
+};
 
+/* GET Bluebird page. */
+router.get('/', function(req, res, next) {
+    //Let's execute the promise
+    addTwoPromise(1)
+        .then(addTwoPromise)
+        .then(addTwoPromise)
+        .then(function(result) {
+            console.log(result); // will print 7 in the console
+            res.render('bluebird', { resultofbb: result });
+        });
 });
 
 module.exports = router;
+
+
